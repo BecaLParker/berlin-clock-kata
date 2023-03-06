51d5b58823e02a67436171a30e1ab3a778c0ae82
Naz.Yilmaz <naz.yilmaz@red-gate.com>
2023-03-06 16:04:18 +0000

    Add rule for SingleMinuteRow

--- CodeKatas/Week2/BerlinClockConverter.cs
+++ CodeKatas/Week2/BerlinClockConverter.cs
@@ -4,7 +4,15 @@ public static class BerlinClockConverter
 {
     public static BerlinClock ConvertDigitalTimeToBerlinClock(DateTime digitalTime)
     {
-        var singleMinuteRow = new SingleMinuteRow(LampState.Off, LampState.Off, LampState.Off, LampState.Off);
+        var minutes = digitalTime.Minute;
+        var remainder = minutes % 5;
+        
+        var singleMinuteRow = new SingleMinuteRow(
+            remainder == 0 ? LampState.Off : LampState.Yellow,
+            remainder <= 1 ? LampState.Off : LampState.Yellow,
+            remainder <= 2 ? LampState.Off : LampState.Yellow,
+            remainder <= 3 ? LampState.Off : LampState.Yellow);
+        
         return new BerlinClock(singleMinuteRow);
     }
 }
