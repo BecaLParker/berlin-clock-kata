dd2c1aa80ad5289201b6ad7c2fc56a22e21af9dc
BecaLParker <beca.parker@red-gate.com>
2023-03-06 14:08:48 +0000

    Pass first test

--- CodeKatas/Week2/BerlinClock.cs
+++ CodeKatas/Week2/BerlinClock.cs
@@ -3,4 +3,9 @@
 public class BerlinClock
 {
     public readonly SingleMinuteRow _singleMinuteRow;
+
+    public BerlinClock(SingleMinuteRow singleMinuteRow)
+    {
+        _singleMinuteRow = singleMinuteRow;
+    }
 }
--- CodeKatas/Week2/BerlinClockConverter.cs
+++ CodeKatas/Week2/BerlinClockConverter.cs
@@ -4,6 +4,7 @@ public static class BerlinClockConverter
 {
     public static BerlinClock ConvertDigitalTimeToBerlinClock(DateTime digitalTime)
     {
-        return new BerlinClock();
+        var singleMinuteRow = new SingleMinuteRow(LampState.Off, LampState.Off, LampState.Off, LampState.Off);
+        return new BerlinClock(singleMinuteRow);
     }
 }
