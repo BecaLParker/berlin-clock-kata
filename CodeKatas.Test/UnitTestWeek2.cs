94eb4e61c99c9269706b790ae8c97ad99be13b7e
BecaLParker <beca.parker@red-gate.com>
2023-03-06 14:22:23 +0000

    Add testcases for SingleMinuteRow Test

--- CodeKatas.Test/UnitTestWeek2.cs
+++ CodeKatas.Test/UnitTestWeek2.cs
@@ -5,14 +5,18 @@ namespace CodeKatas.Test;
 
 public class UnitTestWeek2
 {
-    [Test]
-    public void DigitalTimeToBerlinClock_SingleMinuteRow_Success()
+    [TestCase(0,0,0, LampState.Off, LampState.Off, LampState.Off, LampState.Off)]
+    [TestCase(23,59,59, LampState.Yellow, LampState.Yellow, LampState.Yellow, LampState.Yellow)]
+    [TestCase(12,32,0, LampState.Yellow, LampState.Yellow, LampState.Off, LampState.Off)]
+    [TestCase(12,34,0, LampState.Yellow, LampState.Yellow, LampState.Yellow, LampState.Yellow)]
+    [TestCase(12,35,0, LampState.Off, LampState.Off, LampState.Off, LampState.Off)]
+    public void DigitalTimeToBerlinClock_SingleMinuteRow_Success(int hour, int minutes, int seconds, LampState expectedFirst, LampState expectedSecond, LampState expectedThird, LampState expectedFourth)
     {
-        var digitalTime = new DateTime(2023, 3, 6, 0, 0, 0);
+        var digitalTime = new DateTime(2023, 3, 6, hour, minutes, seconds);
 
         var resultBerlinClock = BerlinClockConverter.ConvertDigitalTimeToBerlinClock(digitalTime);
 
-        var expectedSingleMinuteRow = new SingleMinuteRow(LampState.Off, LampState.Off, LampState.Off, LampState.Off);
+        var expectedSingleMinuteRow = new SingleMinuteRow(expectedFirst, expectedSecond, expectedThird, expectedFourth);
 
         resultBerlinClock._singleMinuteRow.Should().BeEquivalentTo(expectedSingleMinuteRow);
     }
